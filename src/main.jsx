329d5d5df660e55e3c5d7b93919103ad9f42fc0b
munakalasiva <152002974+munakalasiva@users.noreply.github.com>
2024-12-20 11:35:55 +0530

    Update main.jsx
    backend live server added

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:5001";
+export const server = "https://elearning-server-2-we38.onrender.com/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
