103c93585663c871336bf8f357f860688216f15f
munakalasiva <152002974+munakalasiva@users.noreply.github.com>
2024-12-21 14:53:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "https://elearning-server-2-we38.onrender.com/";
+export const server = "https://elearning-server-k2it.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
